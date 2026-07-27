1ea10be985ff1fa1f6a9a4829b34bcc7ac5c5c0f
cfigure <controversialfigure@gmail.com>
2026-07-27 18:20:16 +0100

    Delete src/lumiverse-spindle-types.d.ts

--- src/lumiverse-spindle-types.d.ts
+++
@@ -1,6 +0,0 @@
-declare module 'lumiverse-spindle-types' {
-  export type SpindleFrontendContext = any
-  export type SpindleFloatWidgetHandle = any
-  export type LlmMessageDTO = any
-  export type SpindleAPI = any
-}
